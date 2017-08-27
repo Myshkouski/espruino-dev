952a2721dbb64a601c7b8216d30e4e27a51ed4c9
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2017-08-28 01:01:18 +0300

    index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import { Readable, Writable, Duplex } from 'stream'
+import { Duplex, Writable, Readable } from 'stream'
+
+const d = new Duplex({
+  read() {}, write(d, e, cb) {
+    this.push(d)
+    cb()
+  }
+})
+
+console.log(!!d.read, !!d.write)
+console.log(d instanceof Writable)
+
+d.write('!')
+d.read(10)
+
+alive()
