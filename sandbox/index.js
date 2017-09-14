087660909df616b2ed2147d642ab628ea9c21593
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2017-09-14 23:15:14 +0300

    sandbox

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,1 +1,9 @@
-const ui8a = new Uint8Array([1, 2, 3])
+
+
+setTimeout(() => console.log('timeout'), 0)
+
+process.nextTick(() => console.log('nextTick'))
+
+setImmediate(() => console.log('immediate'))
+
+new Promise(resolve => console.log('promise'))
